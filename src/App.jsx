918f93da812c0824aa53b3821af1f07355ec69ce
Hikusama <leomessi25k@gmail.com>
2025-08-24 14:18:35 +0800

    Format landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import logo from './assets/logo.png'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,28 +7,46 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div><img src={logo} /></div>
-      <h1>SKSalaan</h1>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        {/*  */}
+      </header>
+
+      <main className='text-[#ffffffb6]'>
+        <div id="content" className='relative'>
+          <div className="lgu">
+            <div className="logoG">
+              <img src={logo} alt="" />
+            </div>
+            <ol>
+              <li>SKYouth Announcement</li>
+              <li className='ls'>Salaan</li>
+            </ol>
+          </div>
+          <div className="bgsc">
+            <div className="fp">
+              <div className='wth'></div>
+              <div className='bt4'>
+                <div className="fw">
+                  <h4><span>🎉Welcome</span> to the official announcement page for the youth of Barangay Salaan.</h4>
+                  <button><i className="fas fa-angle-down"></i> See latest update</button>
+                  <p>Stay updated with events, opportunities, and important notices from your SK Council.</p>
+                </div>
+              </div>
+              <div className="backtr relative">
+                <div className="lkd h-[10rem]">
+                  <img src={logo} className='h-[10rem]' />
+                  <h4 className='font-bold text-center !mt-[1rem]'>Updates for the Youths of salaan</h4>
+                </div>
+              </div>
+            </div>
+            {/* <div className="backtrRight"></div> */}
+            <div className="sp">
+
+            </div>
+          </div>
+
+        </div>
+      </main>
     </>
   )
 }
