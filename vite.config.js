c4cda58692c3407f572d82609760ebb5f322499f
Hikusama <leomessi25k@gmail.com>
2025-08-26 15:24:46 +0800

    ready 2nd deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,37 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-    server: {
+  plugins: [react(), tailwindcss(),
+
+  VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'SKSalaan announcement',
+      short_name: 'SKSalaan announcement',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#ffffff',
+      theme_color: '#000000',
+      icons: [
+        {
+          src: '/icon.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/icon.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
+    }
+  })
+  ],
+  server: {
     port: 3000,
   },
 })
